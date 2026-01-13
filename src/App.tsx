07fdda518af7460c380e7613d5ac1d3379cd145f
Sean C Byrd <73047062+byrdsean@users.noreply.github.com>
2026-01-13 10:42:12 -0500

    Adding files to set navigation
    Working on files for setting up the navigation, but can not push code to Github at the moment. So uploading files manually.

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navigation from "../navigation/navigation.tsx"
+
+function App() {
+
+  return (
+      <>
+        <Navigation />
+        <section className="main-content">
+          main content
+        </section>
+      </>
+  )
+}
+
+export default App
