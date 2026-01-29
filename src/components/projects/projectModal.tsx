95dad268b9a459e1382ba15e4af81bbd17ad1dfb
Sean Byrd <byrd.sean1015@gmail.com>
2026-01-28 22:44:18 -0500

    Updated code to link project modals to project description page.

--- src/components/projects/projectModal.tsx
+++ src/components/projects/projectModal.tsx
@@ -1,4 +1,4 @@
-import { type FC } from 'react'
+import type {FC, JSX} from 'react';
 import './ProjectModal.css'
 import type ProjectListItem from "./ProjectListItem.tsx";
 
@@ -9,13 +9,37 @@ const ProjectModal:FC<ProjectListItem> =
     link,
     img: image,
     alt,
+    url,
     description,
     tags
 }) => {
+
+    const parseUrl = (unParsedUrl : string): string => {
+        if (!unParsedUrl) return unParsedUrl;
+
+        const baseUrl = import.meta.env.BASE_URL.toLowerCase();
+
+        if (unParsedUrl.indexOf(baseUrl) === 0) {
+            throw new Error(`Invalid project url provided: ${unParsedUrl}`);
+        }
+
+        return `${baseUrl}projects/${unParsedUrl}`;
+    }
+
+    const wrapLinkToProjectDescription = (children: JSX.Element) : JSX.Element => {
+        return !url ? children : <a href={parseUrl(url)}>{children}</a>;
+    }
+
     return (
         <div className="projectModal" data-id={id}>
-            <img src={image} alt={alt} />
-            <h2>{title}</h2>
+            {wrapLinkToProjectDescription(
+                <img src={image} alt={alt} />
+            )}
+
+            {wrapLinkToProjectDescription(
+                <h2>{title}</h2>
+            )}
+
             {description && <p>{description}</p>}
             {
                 tags && tags.length > 0 &&
